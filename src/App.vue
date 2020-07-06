8b1cc8b45a79bdee49496b7354444f7319622120
xsummersx <605369747@qq.com>
2020-07-06 17:12:47 +0800

    vuexDemo

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/testVuex">testVuex</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
+
 </template>
 
 <style lang="less">
---
+++ src/views/testVuex.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <a>{{this.$store.state.count}}</a>
+    <a>{{counted}}</a>
+    <button @click="add">增加</button>
+  </div>
+
+</template>
+<script>
+export default {
+  name: 'testVuex',
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+    counted () {
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    add () {
+      this.$store.commit('add');
+    }
+  }
+}
+</script>
