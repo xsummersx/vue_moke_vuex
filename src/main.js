f882379c42b2128e0765682b820d26f0fbce433b
xsummersx <605369747@qq.com>
2020-07-06 16:20:06 +0800

    firstCOmit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
-
+Vue.use(VueAxios, Axios)
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,18 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+export default {
+  name: 'about',
+  mounted () {
+    this.init()
+  },
+  methods: {
+    init () {
+      this.axios.get('/data.json').then((res) => {
+        console.log(res)
+      })
+    }
+  }
+}
+</script>
